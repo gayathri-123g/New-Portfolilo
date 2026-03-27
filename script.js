32b0b78367626c4be3cb961048ec370a84294d3c
gayathri <157293016+gayathri-123g@users.noreply.github.com>
2026-03-27 22:01:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ document.getElementById("form").addEventListener("submit", function(e){
     e.preventDefault();
     alert("Recruiter will contact you soon!");
 });
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+        toggleBtn.textContent = "☀️";
+        localStorage.setItem("theme", "light");
+    } else {
+        toggleBtn.textContent = "🌙";
+        localStorage.setItem("theme", "dark");
+    }
+});
